9ec534bc3e47d576e9a019a63c377c3b3f381bee
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-01-22 19:48:07 +0530

    Time: 1 ms (72.91%) | Memory: 43.8 MB (38.95%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,28 +15,22 @@
  */
 class Solution {
      public List<Integer> rightSideView(TreeNode root) {
-             List<Integer> ans = new ArrayList<>();
-             //null root
-             if(root==null) return new ArrayList<>();
-             //get the rightmost ele in lot
-             List<List<Integer>>lot=new ArrayList<>();
-             Queue<TreeNode>q=new LinkedList<>();
-             q.offer(root);
-             while(!q.isEmpty()){
-              List<Integer>tmp=new ArrayList<>();
-              int qsize = q.size();
-              for(int i=0;i<qsize;i++){
-                TreeNode curr = q.poll();
-                tmp.add(curr.val);
-                if(curr.left!=null) q.offer(curr.left);
-                if(curr.right!=null) q.offer(curr.right);
-              }
-              lot.add(new ArrayList<>(tmp));
-             }
-             //select rtmost vals and add to final ans
-             for(List<Integer>al:lot){
-                  ans.add(al.get(al.size()-1));
-                 }
-             return ans;
+        //space-optimized
+      List<Integer>ans=new ArrayList<>();
+      Queue<TreeNode>q=new LinkedList<>();
+      if(root==null) return new ArrayList<>();
+      q.offer(root);
+      while(!q.isEmpty()){
+         int qsize = q.size();
+         List<Integer>tmp=new ArrayList<>();
+         for(int i=0;i<qsize;i++){
+            TreeNode curr = q.poll();
+            tmp.add(curr.val);
+            if(i == qsize-1) ans.add(tmp.get(i));
+            if(curr.left!=null) q.offer(curr.left);
+            if(curr.right!=null) q.offer(curr.right);
+         }
+      }
+      return ans;    
      }
 }
