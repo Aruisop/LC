37045df60d36270b8ca5ca9655a01ce26cd0484d
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-01-27 23:52:37 +0530

    Time: 1 ms (99.53%) | Memory: 47.2 MB (81.25%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,21 +1,16 @@
 class Solution {
-     private static int rec(int nums[],int ind,int memo[]){
-         int n = nums.length;
-         if(ind>=n-1) return 0;
-         if(memo[ind]!=-1) return memo[ind];
-         int min = Integer.MAX_VALUE;
-         int maxJump = nums[ind];
-         for(int j=1;j<=maxJump;j++){
-            if(j+ind<n && rec(nums,j+ind,memo)!=Integer.MAX_VALUE)
-           min = Math.min(min,1+rec(nums,j+ind,memo));
-         }
-         return memo[ind]=min;
-     }
      public int jump(int[] nums) {
-        //memoised soln
       int n = nums.length;
-      int memo[]=new int[n+1];
-      Arrays.fill(memo,-1);
-      return rec(nums,0,memo);   
+      int farthest = 0;
+      int currend = 0;
+      int jumps = 0;
+      for(int i=0;i<n-1;i++){
+       farthest = Math.max(farthest,i+nums[i]);
+       if(i>=currend){
+        jumps++;
+        currend=farthest;
+       }
+      }
+      return jumps;   
      }
 }
