3bd6411cdb93a2d2e098ec0a7febdf83b6ef6b86
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-02-13 20:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (74.46%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,23 @@
 class Solution {
-     private static int rec(int nums[],int ind,int memo[]){
-         int leave = 0;
-         int take = 0;
-         int n = nums.length; 
-         if(ind>=n) return 0;
-         if(memo[ind]!=-1) return memo[ind];
-         take = nums[ind] + rec(nums,ind+2,memo);
-         leave = rec(nums,ind+1,memo);
-         return memo[ind]=Math.max(take,leave);
-     }
+    //  private static int rec(int nums[],int ind,int memo[]){
+    //      int leave = 0;
+    //      int take = 0;
+    //      int n = nums.length; 
+    //      if(ind>=n) return 0;
+    //      if(memo[ind]!=-1) return memo[ind];
+    //      take = nums[ind] + rec(nums,ind+2,memo);
+    //      leave = rec(nums,ind+1,memo);
+    //      return memo[ind]=Math.max(take,leave);
+    //  }
      public int rob(int[] nums) {
-        int n = nums.length;
-        int memo[]=new int[n+1];
-        Arrays.fill(memo,-1);
-        return rec(nums,0,memo);
+         int n = nums.length;
+         int dp[]=new int[n+1];
+         dp[0]=0;
+         dp[1]=nums[0];
+         for(int i=1;i<n+1;i++){
+            if(i>1)
+            dp[i]=Math.max(nums[i-1]+dp[i-2],dp[i-1]);
+         }
+         return dp[n];
      }
 }
