eb51c3c4ffbab1bebcf14c28d5007b8a4b6ec96d
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-02-15 09:41:37 +0530

    Time: 7 ms (72.49%) | Memory: 43.1 MB (77.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,30 +1,15 @@
 class Solution {
-    //  private static int rec(int amount, int[] coins,int ind,int memo[][]){
-    //    int n = coins.length;
-    //    if(ind>=n) return 0;
-    //    if(amount==0) return 1;
-    //    if(memo[amount][ind]!=-1) return memo[amount][ind];
-    //    int nottake = rec(amount,coins,ind+1,memo);
-    //    int take=0;
-    //    if(amount-coins[ind]>=0)
-    //    take = rec(amount-coins[ind],coins,ind,memo);
-    //    return memo[amount][ind]=nottake+take; 
-    //  }
      public int change(int amount, int[] coins) {
-         //pure dp (intuitive)
+         //space optimised(unintuitive if 2d dp isnt covered).
          int n = coins.length;
-         int dp[][]=new int[amount+1][n+1];
-         //for amount 0, all j indices (for coins) will have only 1 possiblity.
-         for(int j=0;j<n+1;j++){
-            dp[0][j]=1;
-         }
-         for(int i=1;i<amount+1;i++){
-             for(int j=1;j<n+1;j++){
-              int leave = dp[i][j-1],take=0;
-              if(i-coins[j-1]>=0) take = dp[i-coins[j-1]][j];   
-              dp[i][j]=leave+take;
+         int dp[]=new int[amount+1];
+         dp[0]=1;
+         for(int c:coins){
+             for(int i=0;i<amount+1;i++){
+              if(i-c>=0)  
+              dp[i]+=dp[i-c];
              }
          }
-         return dp[amount][n];
+         return dp[amount];
      }
 }
