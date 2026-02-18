696c808999102d6c3bdf130763f4988a14271ea6
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-02-18 11:00:26 +0530

    Time: 2 ms (95.08%) | Memory: 46 MB (19.93%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,22 @@
 class Solution {
-     private static void rec(int[] candidates,List<List<Integer>>ans,List<Integer>tmp,int ind,int target){
+     private static void btrack(int[] candidates, int target,List<Integer>tmp,List<List<Integer>> ans,int ind){
          int n = candidates.length;
-         if(ind>=n || target<0) return;
+         if(target<0) return;
          if(target==0){
             ans.add(new ArrayList<>(tmp));
             return;
          }
-         tmp.add(candidates[ind]);
-         rec(candidates,ans,tmp,ind,target-candidates[ind]);
-         tmp.remove(tmp.size()-1);
-         rec(candidates,ans,tmp,ind+1,target);
+         for(int i=ind;i<n;i++){ 
+           tmp.add(candidates[i]);
+           btrack(candidates,target-candidates[i],tmp,ans,i);
+           tmp.remove(tmp.size()-1);  
+         }
      }
      public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>ans = new ArrayList<>();
-        List<Integer>tmp=new ArrayList<>();
-        rec(candidates,ans,tmp,0,target);
-        return ans;
+          //realised that one small base case is crucial, rest everything follows std template. 
+           List<Integer>tmp=new ArrayList<>();
+           List<List<Integer>> ans = new ArrayList<>();
+           btrack(candidates,target,tmp,ans,0);
+           return ans;
      }
 }
