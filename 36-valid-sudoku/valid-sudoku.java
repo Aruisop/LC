31331eb988009828448e6615e5866edd516ddb43
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-01 19:55:22 +0530

    Time: 12 ms (16.71%) | Memory: 52.5 MB (8.25%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,20 +1,22 @@
 class Solution {
      public boolean isValidSudoku(char[][] board) {
-      boolean row[][]=new boolean[9][9];
-      boolean col[][]=new boolean[9][9]; 
-      boolean thrxthr[][]=new boolean[9][9];
+      //This is Brute-Force, space un-optimised
+      //Hash re-comp and StringBuild up push sc to O(n^2)
+      //whereas the TC remains same at O(81) ~ O(1), for N*N O(N^2)
+      HashSet<String>hs=new HashSet<>();
       for(int i=0;i<9;i++){
          for(int j=0;j<9;j++){
-          if(board[i][j]!='.'){   
-          int num = board[i][j]-'1';
-          int boxidx = (i/3)*3+j/3;
-          if(row[i][num] || col[j][num] || thrxthr[boxidx][num]) return false;
-          row[i][num]=true;
-          col[j][num]=true;
-          thrxthr[boxidx][num]=true;
-          }
+          char num = board[i][j];
+          if(num=='.') continue;
+          String row = new String(num+"in row"+i);
+          String col = new String(num+"in col"+j);
+          String thrxthrbox = new String(num+"in thrxthrbx"+i/3+j/3);
+          if(hs.contains(row) || hs.contains(col) || hs.contains(thrxthrbox)) return false;
+          hs.add(row);
+          hs.add(col);
+          hs.add(thrxthrbox);
          }
       }
-      return true;   
+      return true;
      }
 }
