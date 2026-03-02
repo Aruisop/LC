94a924098ec4cb78882d95200a55f0bc17980ce0
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-02 20:18:45 +0530

    Time: 17 ms (97.93%) | Memory: 45.3 MB (86.67%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,65 +1,32 @@
-class DSU{
-     int parent[];
-     int rank[];
-     DSU(int n){
-      parent=new int[n];
-      rank=new int[n];
-      for(int i=0;i<n;i++){
-        parent[i]=i;
-        rank[i]=0;
-      }
-     }
-     int find(int x){
-        if(parent[x]!=x) parent[x]=find(parent[x]);
-        return parent[x];
-     }
-     boolean union(int x, int y){
-         int px = find(x);
-         int py = find(y);
-         if(px==py) return false;
-         if(rank[px]<rank[py]){
-            parent[px]=py;
-         }else if(rank[py]<rank[px]){
-            parent[py]=px;
-         }else{
-            parent[py]=px;
-            rank[px]++;
-         }
-         return true;
-     } 
- }
 class Solution {
      public int minCostConnectPoints(int[][] points) {
-      //BFORCE DSU+Kruskals, main bottleneck over here is sorting edges 
-      //TC: O(N^2*log(N^2))  ~ O(E*log(E)) ~ O(2*N^2*log(N)) ~ O(N^2*log(N))
-      //SC: O(N^2) (list building using the edges)
-      //Optimization here is using Prim's Algo 
-      List<int[]> e = new ArrayList<>();
-      int n = points.length;
-      for(int i=0;i<n;i++){
-         for(int j=i+1;j<n;j++){
-           int mDist = Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
-           e.add(new int[]{mDist,i,j});
+         //Prims optimal algo(non-inutuit; revise Algo)
+         // TC: O(n^2)
+         // SC: O(n^2)
+         int n = points.length;
+         int minDist[]=new int[n];
+         Arrays.fill(minDist,Integer.MAX_VALUE);
+         minDist[0]=0;
+         boolean vis[]=new boolean[n];
+         int calcCost=0; 
+         for(int i=0;i<n;i++){
+             int u =-1;
+             //find proper u, with minm dist
+             for(int j=0;j<n;j++){
+                 if(!vis[j] && (u==-1 || minDist[j]<minDist[u])){
+                    u=j;
+                 }
+             }
+             vis[u]=true;
+             calcCost+=minDist[u];
+             //build up the minm dist array
+             for(int v=0;v<n;v++){
+                 if(!vis[v]){
+                    int currDist = Math.abs(points[u][0]-points[v][0])+Math.abs(points[u][1]-points[v][1]);
+                    minDist[v] = Math.min(currDist,minDist[v]); 
+                 }
+             }
          }
-      }
-      Collections.sort(e,(a,b)->(a[0]-b[0]));
-      DSU dsu = new DSU(n);
-      int collectedCost = 0;
-      int usedEdges = 0;
-      for(int edge[]:e){
-         int cost = edge[0];
-         int u = edge[1];
-         int v = edge[2];
-         //ensure that union works, ie u and v are not alr conn
-         //basically not part of the same comp
-         if(dsu.union(u,v)){
-            //take the cost and connect u and v
-           collectedCost+=cost; 
-           usedEdges++;
-         }
-         //has only n-1 edges
-         if(usedEdges==n-1) break;
-      }
-      return collectedCost;     
+         return calcCost;   
      }
 }
