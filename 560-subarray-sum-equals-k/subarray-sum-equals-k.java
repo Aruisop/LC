6abc516dbb4ad6c88bd28398a0ff22742320dc17
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-03 13:35:13 +0530

    Time: 22 ms (96.22%) | Memory: 48.4 MB (94.20%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,17 +1,25 @@
 class Solution {
      public int subarraySum(int[] nums, int k) {
-        //Bforce,Unoptimal 
-        //TC: O(n^2)
-        //SC: O(1)
-      int ct = 0;
+      //Optimal solution w/ HashMap
+      //idea is to ct the prefsums at each instance 
+      //and check if prefsum-k exists in the hashmap
+      //for a prefix sum ==0 always 1 subarr exists (hm.put(0,1))
+      //Unintuit AF.
+      //TC: O(n)
+      //SC: O(1)
+      int ct=0;
+      HashMap<Integer,Integer>hm=new HashMap<>(); 
+      hm.put(0,1);
+      int presum=0;
       int n = nums.length;
       for(int i=0;i<n;i++){
-          int sum = 0;
-         for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k) ct++; 
+         presum+=nums[i];
+         if(hm.containsKey(presum-k)){
+          ct+=hm.get(presum-k);
          }
+         //put the presum into the hashmap, and ct its freq
+         hm.put(presum,hm.getOrDefault(presum,0)+1);
       }
-     return ct;
+      return ct;   
      }
 }
