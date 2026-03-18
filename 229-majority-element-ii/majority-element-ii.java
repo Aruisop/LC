854b358b153264e1aa1de09fb26ed9cf57afe548
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-19 01:31:34 +0530

    Time: 4 ms (59.75%) | Memory: 52.9 MB (26.60%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,35 @@
 class Solution {
      public List<Integer> majorityElement(int[] nums) {
-     //TC: O(n), BRUTE FORCE
-     //SC: O(n)   
-     int n = nums.length;
-     HashMap<Integer,Integer>hm=new HashMap<>();
-     for(int ele:nums){
-     hm.put(ele,hm.getOrDefault(ele,0)+1);
-     }
-     List<Integer>ans=new ArrayList<>();
-     for(int i=0;i<n;i++){
-        if(!ans.contains(nums[i]) && hm.get(nums[i])>n/3) ans.add(nums[i]);
-     }
-     return ans;
+         //TC: O(n)
+         //SC: O(1)----> Space Optimised + Highly Unituitive Boyer Moore Voting Algo :(   
+         //idea is to identify that for eles that appear more than n/k times, the possible number is k-1
+         //therefore 2 candidates
+         int n = nums.length;
+         Integer cand1=null,cand2=null;
+         int ct1=0,ct2=0;
+         List<Integer>ans = new ArrayList<>();
+         for(int ele:nums){
+          if(cand1!=null && cand1==ele) ct1++;
+          else if(cand2!=null && cand2==ele) ct2++;
+          else if(ct1==0){
+            cand1 = ele;
+            ct1 = 1;
+          }else if(ct2==0){
+             cand2 = ele;
+             ct2 = 1;
+          }else{
+            ct1--;
+            ct2--;
+          }
+         }
+         ct1=0;
+         ct2=0;
+         for(int ele:nums){
+            if(cand1==ele) ct1++;
+            else if(cand2==ele) ct2++;
+         }
+         if(ct1>n/3) ans.add(cand1);
+         if(ct2>n/3) ans.add(cand2);
+         return ans;    
      }
 }
