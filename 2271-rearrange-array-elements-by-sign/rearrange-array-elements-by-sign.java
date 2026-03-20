72032c964547ce2fbd5238478d3c9af70fca0e1f
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-20 10:14:01 +0530

    Time: 7 ms (15.40%) | Memory: 175.4 MB (72.27%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,27 +1,25 @@
 class Solution {
      public int[] rearrangeArray(int[] nums) {
+         int s1 = 0;
+         int s2 = 0;
          int n = nums.length;
-         //n is even
-         //n/2 odd and even
-         //TC; O(n)
-         //SC: O(n)
          int ans[]=new int[n];
          int k = 0;
-         List<Integer>pos=new ArrayList<>();
-         List<Integer>neg=new ArrayList<>();
-         for(int i=0;i<n;i++){
-           if(nums[i]>0) pos.add(nums[i]);
-           else neg.add(nums[i]);
-         }
-         //pos
-         int s1=0;
-         //neg
-         int s2=0;
-         while(s1<n/2 && s2<n/2){
-            ans[k++]=pos.get(s1);
-            ans[k++]=neg.get(s2);
-            s1++;
-            s2++;
+         int prev = 0;
+         while(s1<n && s2<n){
+          while(nums[s1]<0 && prev<=0) s1++;
+          if(nums[s1]>0 && prev<=0){
+             ans[k++]=nums[s1];
+             prev=nums[s1];
+             s1++;
+             }
+             while(nums[s2]>0 && prev>=0) s2++;
+             if(nums[s2]<0 && prev>=0){
+             ans[k++]=nums[s2];
+              prev=nums[s2];
+              s2++;
+             }
+             
          }
          return ans;
      }
