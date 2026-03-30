ce5b403e948401a070556e8a81272ec4d3388a46
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-03-30 18:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (63.06%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,17 +1,29 @@
 class Solution {
      public int maxProduct(int[] nums) {
-      //TC: O(n^2)
-      //SC: O(1)  
-      int n = nums.length;
-      if(n==0) return nums[0];
-      int max = Integer.MIN_VALUE;
-      for(int i=0;i<n;i++){
-         int subpr = 1;
-         for(int j=i;j<n;j++){
-           subpr*=nums[j];
-           max = Math.max(subpr,max);
+         //TC: O(n)
+         //SC: O(2*n) ~ O(n) (space un-optimised)
+         int n = nums.length;
+         int preprod[]=new int[n];
+         int posprod[]=new int[n];
+         int pp=1;
+         int posp=1;
+         //prefix prod
+         for(int i=0;i<n;i++){
+         pp = pp*nums[i];
+         preprod[i] = pp;
+         //reset pp if alr 0
+         if(pp==0) pp=1;
          }
-      }
-      return max;   
+         for(int j=n-1;j>=0;j--){
+            posp=posp*nums[j];
+            posprod[j]=posp;
+            if(posp==0) posp=1;
+         }
+         //trav thru and find max
+         int max = Integer.MIN_VALUE;
+         for(int i=0;i<n;i++){
+           max = Math.max(max, Math.max(preprod[i], posprod[i]));
+         }
+         return max;    
      }
 }
