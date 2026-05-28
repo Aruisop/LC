aae9d39868b5a253318b691dadf7b31b6b7bfbf1
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-05-28 12:51:02 +0530

    Time: 7 ms (21.51%) | Memory: 53.6 MB (73.98%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,21 +1,16 @@
 class Solution {
      public List<Integer> grayCode(int n) {
-         //Bforce
-         //TC: O(n^2) 
+         //Optimal
+         //TC: O(n) 
          //SC: O(n)
-         List<Integer>curr = new ArrayList<>();
-         curr.add(0);
-         for(int i=0;i<n;i++){
-             List<Integer>next  = new ArrayList<>();
-             for(int ele:curr){
-              next.add(ele);
-              }
-              int add = 1<<i;
-             for(int j=curr.size()-1;j>=0;j--){
-               next.add(curr.get(j)+add); 
-             }
-             curr = next; 
+         List<Integer>ans = new ArrayList<>();
+         ans.add(0);
+         //idea is to not use the reversal technique
+         //and use bit manipulation
+         for(int i=1;i<Math.pow(2,n);i++){
+             //xor btw the curr and prev vals 
+             ans.add(i^(i>>1));
           }
-         return curr;  
+         return ans;  
      }
 }
