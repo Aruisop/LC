b50022b395d9925e813dc7dfb3eb49802a1f0b21
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-05-28 19:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (25.32%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,25 +1,15 @@
 class Solution {
      public boolean check(int[] nums) {
-         //TC: O(n^2)
-         //SC: O(n)
+         //Optimised
+         //TC: O(n)
+         //SC: O(1)
          int n = nums.length;
-         int b[] = new int[n];
+         int drop = 0;
          for(int i=0;i<n;i++){
-            b[i]=nums[i];
+          if(nums[i]>nums[(i+1)%n]) drop++;
          }
-         //prev submit didnt work for duplis
-         Arrays.sort(nums);
-         for(int x=0;x<n;x++){
-             if(b[0]!=nums[x]) continue;
-             boolean flag = true;
-             for(int i=0;i<n;i++){
-               if(b[i]!=nums[(i+x)%n]){
-                flag=false;
-                break;
-               }
-             }
-             if(flag==true) return true;
-         }
-        return false; 
+         //the number of such drops can only be 1 or 0.
+         //for rotated and sorted
+         return drop<=1;
      }
 }
