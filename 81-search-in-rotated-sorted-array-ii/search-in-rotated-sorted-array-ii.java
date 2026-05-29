4b36bc6d83133d8c0dc9f8b7e7901a557cda91f1
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-05-29 11:12:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (44.49%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -2,7 +2,7 @@ class Solution {
      public boolean search(int[] nums, int target) {
          //TC: O(logN)
          //SC: O(1)
-         //Approach 1
+         //Approach 2
          int n = nums.length;
          int l = 0;
          int r = n-1;
@@ -10,14 +10,18 @@ class Solution {
              int mid = l+(r-l)/2;
              if(nums[mid]==target) return true;
              //nums[l]<nums[mid] not <= as in type 1 of this problem
-             if(nums[l]<nums[mid]){
-                if(nums[l]<=target && target<nums[mid]) r=mid-1;
-                else l=mid+1;
-             }else if(nums[l]>nums[mid]){
-                if(nums[mid]<target && target<=nums[r]) l=mid+1;
-                else r=mid-1;
+             if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+                 //solve problem based on ambiguous case  
+                 l++;
+                 r--;
+             }else if(nums[l]<=nums[mid]){
+                 //lhs sorted first
+                 if(nums[l]<=target && target<nums[mid]) r=mid-1;
+                 else l=mid+1;
              }else{
-                  l++;
+                 //rhs sorted first
+                 if(nums[mid]<target && target<=nums[r]) l=mid+1;
+                 else r=mid-1;
              }
          }
          return false;
