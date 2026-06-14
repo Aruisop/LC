20b5f369a4c0dd1bc2a3a8eb387ec1bade6ef23d
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-06-14 12:55:00 +0530

    Time: 5 ms (58.50%) | Memory: 100.8 MB (94.24%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -9,35 +9,35 @@
  * }
  */
 class Solution {
+     private static ListNode rev(ListNode head){
+         ListNode prev = null;
+         ListNode curr = head;
+         while(curr!=null){
+             ListNode nxt = curr.next;
+             curr.next = prev;
+             prev = curr;
+             curr = nxt;
+         } 
+         return prev;
+      }
      public int pairSum(ListNode head) {
-          //BFORCE, unoptimal
-          //TC: O(n)
-          //SC: O(n)
-         if(head==null) return -1;
-         List<Integer>al = new ArrayList<>();
-         ListNode temp = head;
-         while(temp!=null){
-          al.add(temp.val);  
-          temp = temp.next;
+         //Optimal
+         //TC: O(n)
+         //SC: O(1)
+         ListNode slow = head;
+         ListNode fast = head;
+         while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
          }
-         //got the size{even}
-         int n = al.size();
-         //next identify valid nodes+twins, and store their sums
-         //find the sum and store for each node
-         List<Integer>twinsum = new ArrayList<>();
-         HashSet<Integer>hs=new HashSet<>();
-         for(int i=0;i<n;i++){
-             int twinind = n-1-i;
-             if(twinind>=0 && twinind<n && !hs.contains(twinind)){
-                twinsum.add(al.get(i)+al.get(twinind));
-                hs.add(i);
-                hs.add(twinind);
-             }
+         ListNode second = rev(slow);
+         ListNode first = head;
+         int maxsum  = Integer.MIN_VALUE;
+         while(second!=null){
+            maxsum = Math.max(maxsum,first.val+second.val);
+            first=first.next;
+            second=second.next;
          }
-         int max = Integer.MIN_VALUE;
-         for(int ele:twinsum){
-           max = Math.max(max,ele);
-         }
-         return max;
+         return maxsum;
      }
 }
