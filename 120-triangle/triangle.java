51f7e74f3df228a0a46e5f7ceda1a9f4e517989b
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-06-17 13:49:37 +0530

    Time: 3 ms (83.10%) | Memory: 46.7 MB (7.69%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -3,9 +3,9 @@ class Solution {
          int n = triangle.size();
          if(i==n-1) return triangle.get(i).get(j);
          if(memo[i][j]!=null) return memo[i][j];
-         int nextRowSameCol = rec(triangle,i+1,j,memo);
-         int nextRowNextCol = rec(triangle,i+1,j+1,memo);
-         return memo[i][j] = triangle.get(i).get(j) + Math.min(nextRowSameCol,nextRowNextCol);
+         int nextRowSameCol = triangle.get(i).get(j)+rec(triangle,i+1,j,memo);
+         int nextRowNextCol = triangle.get(i).get(j)+rec(triangle,i+1,j+1,memo);
+         return memo[i][j] =  Math.min(nextRowSameCol,nextRowNextCol);
      }
      public int minimumTotal(List<List<Integer>> triangle) {
        int m = triangle.size();
