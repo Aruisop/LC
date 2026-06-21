b8148033d233c799a3c77fe7482876f9faa569a3
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-06-21 19:16:57 +0530

    Time: 57 ms (5.15%) | Memory: 101.1 MB (5.04%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,32 +1,25 @@
 class Solution {
      public int maxIceCream(int[] costs, int coins) {
-         //Without counting sort
-         //TC: O(nlogn)
-         //SC: O(1)
-         int min = Integer.MAX_VALUE;
-         for(int ele:costs){
-           min =  Math.min(min,ele);
+         //counting sort approach
+         //TC: O(n)
+         //SC: O(n)
+         HashMap<Integer,Integer>hm = new HashMap<>();
+         int max = Integer.MIN_VALUE;
+         for(int cost:costs){
+            hm.put(cost,hm.getOrDefault(cost,0)+1);
+            max = Math.max(max,cost);
          }
-         if(coins<min) return 0;
-         Arrays.sort(costs);
-
-          //[1,1,2,3,4]
-          //[1,2,4,7,11]
-
-          //[1,1,2,3,5,6]
-          //[1,2,4,7,12,18]
-
-          //[2,3,3,6,6,6,7,9,10] 
-          //[2,5,8,14,20,26,32,39,48,58] 
-          int n = costs.length;
-          long presum = 0; 
-          long ct=0;
-         for(int i=0;i<n;i++){
-             presum+=costs[i];
-             if(presum<=coins){
-                ct++;
-             }
+         //counting sort starts here
+         //test each cost value, from 1 to the max cost
+         int bars = 0;
+         for(int i=1;i<=max;i++){
+             if(!hm.containsKey(i)) continue;
+             int ct = hm.get(i);
+             int potential_bar = Math.min(ct,coins/i);
+             bars+=potential_bar;
+             coins-=potential_bar*i;
+             if(coins<ct) continue;
          }
-          return (int)ct;
+         return bars;
      }
 }
