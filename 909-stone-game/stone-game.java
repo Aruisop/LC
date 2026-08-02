6bb3b78913193ccede36cd423f4b4873aee30f1b
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-08-02 11:01:23 +0530

    Time: 8 ms (18.78%) | Memory: 46.8 MB (16.42%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,20 +1,16 @@
 class Solution {
-     //base-level recursion follows : LC 486
-     private static int rec(int l, int r, int piles[],int memo[][]){
-        if(l==r) return memo[l][r] = piles[l];
-        if(memo[l][r]!=-1) return memo[l][r];
-        return memo[l][r] = Math.max( piles[l]-rec(l+1,r,piles,memo) , piles[r]-rec(l,r-1,piles,memo) );
-     } 
      public boolean stoneGame(int[] piles) {
-         //Slightly Optimised, Memoised
-         //{core idea: recurse based on finding the difference btw p1 and p2 scores}
-         //TC: O(n^2)
-         //SC: O(n)  
          int n = piles.length;
-         int memo[][]=new int[n][n];
-         for(int mem[]:memo){
-            Arrays.fill(mem,-1);
+         int dp[][]=new int[n][n];
+         for(int i=0;i<n;i++){
+         dp[i][i] = piles[i];
          }
-         return rec(0,n-1,piles,memo)>=0;
+         for(int leng=2;leng<=n;leng++){
+             for(int l=0;l<n-leng+1;l++){
+               int r = leng+l-1;
+               dp[l][r] = Math.max(piles[l]-dp[l+1][r],piles[r]-dp[l][r-1]);
+             } 
+         }
+         return dp[n-1][n-1]>=0;   
      }
 }
