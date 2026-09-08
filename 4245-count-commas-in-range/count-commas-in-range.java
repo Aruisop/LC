9f00dab30f935ac40379ca82949ed48406eae3b2
Aarya Shah <140880130+Aruisop@users.noreply.github.com>
2026-09-08 20:56:36 +0530

    Time: 4 ms (16.64%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,22 +1,18 @@
 class Solution {
-     private static int ctDigs(int n){
-         int ct_digs = 0;
+     public int countCommas(int n) {
+         //slight optimisation
+         //TC:O(n)
+         //SC: O(1)
          int temp = n;
+         int ct_digs = 0;
          while(temp!=0){
             temp/=10;
             ct_digs++;
          }
-         return ct_digs; 
-     } 
-     public int countCommas(int n) {
-         //bruforce
-         //TC:O(n*n)
-         //SC: O(1)
+         if(ct_digs<4) return 0;
          int count = 0;
-         for(int i=1;i<=n;i++){
-          int ct = ctDigs(i);
-          if(ct<4) continue;
-          else count++;
+         for(int i=n;i>=1000;i--){
+               count++;
          }   
          return count;
      } 
